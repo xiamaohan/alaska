f7aa7eb68244fa038b7663236c13de4133da8d94
谢小旭 <sunx@jee.io>
2018-11-30 17:41:26 +0800

    Signed-off-by: 谢小旭 <sunx@jee.io>

--- alaska-core/src/main/java/io/jee/alaska/model/page/PageInputDataTable.java
+++ alaska-core/src/main/java/io/jee/alaska/model/page/PageInputDataTable.java
@@ -2,6 +2,7 @@ package io.jee.alaska.model.page;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 
@@ -31,8 +32,11 @@ public class PageInputDataTable {
         String orderDir = request.getParameter("order[0][dir]");//排序的顺序asc or desc
         String orderColumn = request.getParameter("columns["+order+"][data]");//排序的列。注意，我认为页面上的列的名字要和表中列的名字一致，否则，会导致SQL拼接错误
 		Direction direction = Direction.ASC;
-		if(orderDir.equalsIgnoreCase("desc")){
+		if(StringUtils.isNotBlank(orderDir) && orderDir.equalsIgnoreCase("desc")){
 			direction = Direction.DESC;
+		}
+		if(StringUtils.isBlank(orderColumn)) {
+			return PageRequest.of(start/length, length);
 		}
 		return PageRequest.of(start/length, length, direction, orderColumn);
 	}
